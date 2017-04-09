d0aca0c799088285d3c43486bf7d6efaae18cd1c
zertu <290055513@qq.com>
2017-04-09 22:06:53 +0800

    更新build,样式全部逻辑完善

--- src/js/index.js
+++ src/js/index.js
@@ -4,34 +4,45 @@ const fs = require('fs'),
     getTime = require('./src/js/Data/getTime')
 window.onload = () => {
     const chart = echarts.init(document.getElementById('data'))
-    document.getElementById('start').addEventListener('click', async function () {
-            let result
+    const startBottom = document.getElementById('start')
+    startBottom.addEventListener('click', async function () {
+        let result
+        try {
+            let timer = setInterval(async() => {
+                let data = await readFile('timeline.txt')
+                data = data.split('，')
+                getUrl.series[0].data = data.map((i, index) => i.split(',')[1] - (data[index - 1]
+                    ? data[index - 1].split(',')[1]
+                    : 0))
+                getUrl.xAxis.data = data.map(i => i.split(',')[0])
+                getUrl.dataZoom[0].startValue = getTime().toString()
+                chart.setOption(getUrl)
+            }, 5000)
+            result = await start()
+            clearInterval(timer)
+            timer = null
+        } catch (e) {}
+        if (result.length) {
+            startBottom.value='读取URL'
             try {
-               let timer= setInterval(async()=>{
-                    let data=await readFile('timeline.txt')
-                    data=data.split('，')
-                    getUrl.series[0].data=data.map((i,index)=>i.split(',')[1]-(data[index-1]?data[index-1].split(',')[1]:0))
-                    getUrl.xAxis.data=data.map(i=>i.split(',')[0])
-                    getUrl.dataZoom[0].startValue=getTime().toString()
-                    chart.setOption(getUrl)
-                },5000)
-                result = await start()
-                clearInterval(timer)
-                timer=null
+                let data = await readurl()
+            startBottom.value='爬取评论'
+           await pagereader(data)
+           startBottom.value='爬取完毕'
+           startBottom.disabled=true
             } catch (e) {
+                console.log(e)
             }
-            if (result.length) {
-                
-            } else {
-                chart.hideLoading()
-            }
-        }, false)
+        } else {
+            chart.hideLoading()
+        }
+    }, false)
 }
 
-function readFile(filename){
-    return new Promise((res,rej)=>{
-        fs.readFile(filename,'utf-8',(err,data)=>{
-            if(err){
+function readFile(filename) {
+    return new Promise((res, rej) => {
+        fs.readFile(filename, 'utf-8', (err, data) => {
+            if (err) {
                 rej(err)
             }
             res(data)
--- src/js/tableData/index.js
+++
@@ -1,6 +0,0 @@
-const {observable}= require('mobx')
-
-export const time = observable({
-    firstName: "Clive Staples",
-    lastName: "Lewis"
-})
