9f172ffd8887b2e28bba4ec0fb694ca738d6b594
zertu <290055513@qq.com>
2017-04-16 22:39:46 +0800

    fixbug

--- ml/src/index.js
+++ ml/src/index.js
@@ -1,13 +1,27 @@
 const http = require('http'),
     ctx = document.getElementById('ctx')
-ctx.addEventListener('click', e => {
-    http.get('http://139.224.232.97:8080?ctx=' + ctx.value, res => {
-        let result = ''
-        res.on('data', data => result += data)
-        res.on('end', () => {
-            document
-                .querySelector('p')
-                .innerHTML = result
+document
+    .getElementById('submit')
+    .addEventListener('click', e => {
+        const url = 'http://139.224.232.97:8080?ctx=' + encodeURI(ctx.value)
+        console.log(url)
+        http.get(url, res => {
+            let result = ''
+            res.on('data', data => result += data)
+            res.on('end', () => {
+                switch (result.trim()) {
+                    case 'positive':
+                        document
+                            .querySelector('p')
+                            .innerHTML = '积极情绪'
+                        break
+                    case 'negative':
+                        document
+                            .querySelector('p')
+                            .innerHTML = '消极情绪'
+                        break
+                }
+
+            })
         })
-    })
-}, false)
+    }, false)
